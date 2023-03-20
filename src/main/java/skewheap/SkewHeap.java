b3c80d22491befd0deeaa0a1468643326a3cd249
Artyom Artyomkin <welllet@mail.ru>
2023-03-20 21:40:40 +0300

    added breadcrumbs

--- src/main/java/skewheap/SkewHeap.java
+++ src/main/java/skewheap/SkewHeap.java
@@ -11,6 +11,7 @@ public class SkewHeap {
         Node right;
 
         Node(Double value) {
+            System.out.print("C_");
             this.value = value;
             this.left = null;
             this.right = null;
@@ -18,53 +19,63 @@ public class SkewHeap {
     }
 
     public SkewHeap() {
-
     }
 
     public void add(Double value) {
-        Node current = new Node(value);
-        root = merge(root,current);
+        System.out.print("A_");             //1
+        Node current = new Node(value);     //1
+        root = merge(root,current);         //1
     }
 
     public Double removeMin() {
-        if(empty()){
-            throw new NoSuchElementException();
-        }
-        else{
-            Double min = root.value;
-            root = merge(root.left, root.right);
-            return min;
-        }
+        System.out.print("RM_");                //1
+        if(empty()){                            //2
+            System.out.print("E_");             //3
+            throw new NoSuchElementException(); //3
+        } else {                                //4
+            System.out.print("NE_");            //4
+            Double min = root.value;            //4
+            root = merge(root.left, root.right);//4
+            return min;                         //4
+        }                                       //5
     }
 
     public boolean empty() {
-        return root == null;
+        return root == null; //1
     }
 
     public void merge(SkewHeap other) {
-        if(other != null){
-            this.root = merge(this.root, other.root);
-            other.root = null;
-        }
+        System.out.print("MH_");                        //1
+        if(other != null) {                             //2
+            System.out.print("MHNE_");                  //3
+            this.root = merge(this.root, other.root);   //3
+            other.root = null;                          //3
+        } else {                                        //4
+            System.out.print("MHE_");                   //4
+        }                                               //5
     }
 
     public Node merge(Node root1, Node root2) {
-        Node firstRoot = root1;
-        Node secondRoot = root2;
-        if(firstRoot == null){
-            return secondRoot;
-        }else if(secondRoot == null){
-            return firstRoot;
-        }
-        if(firstRoot.value <= secondRoot.value){
-            Node temp = firstRoot.right;
-            firstRoot.right = firstRoot.left;
-            firstRoot.left = merge(secondRoot,temp);
-            return firstRoot;
-        }
-        else{
-            return merge(secondRoot,firstRoot);
-        }
+        System.out.print("MN_");                        //1
+        Node firstRoot = root1;                         //1
+        Node secondRoot = root2;                        //1
+        if(firstRoot == null){                          //2
+            System.out.print("MNFN_");                  //3
+            return secondRoot;                          //3
+        }else if(secondRoot == null){                   //4
+            System.out.print("MNSN_");                  //5
+            return firstRoot;                           //5
+        }                                               //6
+        if(firstRoot.value <= secondRoot.value){        //7
+            System.out.print("MNF<=S_");                //8
+            Node temp = firstRoot.right;                //8
+            firstRoot.right = firstRoot.left;           //8
+            firstRoot.left = merge(secondRoot,temp);    //8
+            return firstRoot;                           //8
+        } else {                                        //9
+            System.out.print("MNF>S_");                 //9
+            return merge(secondRoot,firstRoot);         //9
+        }                                               //10
 
     }
 }
